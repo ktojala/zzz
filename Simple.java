44fb552c3792ac3485849e394e6c29fd7b6f93f3
kojal <k.ojala@kolumbus.fi>
2021-03-13 20:12:57 +0200

    Lisättiin Simple.java -tiedosto

---
+++ Simple.java
@@ -0,0 +1,6 @@
+class Simple{  
+    public static void main(String args[]) {  
+     System.out.println("Hello Java");  
+    }
+// Havainto: 2015 kirjoitettiin Netbeansiin   main(String[] args)
+} 
